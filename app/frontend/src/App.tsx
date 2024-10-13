a907f9b71452dfba44ed3eb122ef08de2211c6f7
yassine-RM <y.remmani@autodealersdigital.com>
2024-10-13 11:50:17 +0100

    Sync local with container changes

--- app/frontend/src/App.tsx
+++ app/frontend/src/App.tsx
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn React sss cc dd
         </a>
       </header>
     </div>
